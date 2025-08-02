668de9cb6645688ffa9b0e35c108579b35c2483e
mohd kaif <mk2818356@gmail.com>
2025-08-02 20:43:27 +0530

    complete week_02

--- week_02/index.ts
+++ week_02/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// it will converth "hello" into array of bytes[104,101,108,111,103] 
+const binaryRepresenting = new TextEncoder().encode("hello") 
+
+console.log(binaryRepresenting)
+console.log(typeof(binaryRepresenting))
+const a = [104]
+console.log(typeof(a))
+
+
+// hexa take only 4 bit from 0 to f which i make the all bit 1111 => 15 == f
+
+// base64 take 6 bit from the byte it has value A-Z, a-z, 0-9, "+" , "/" if i add all this it's total count is 64
