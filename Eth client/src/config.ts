4e87013226de8baba4426ace8f7f811279bb4dea
nobitakaif <mk2818356@gmail.com>
2025-09-11 14:23:06 +0000

    initialize eht client

---
+++ Eth client/src/config.ts
@@ -0,0 +1,14 @@
+
+import { createConfig,http } from "wagmi"
+import { mainnet } from "wagmi/chains"
+import { injected } from "wagmi/connectors"
+
+export const config = createConfig({
+  chains : [mainnet],
+  connectors : [
+    injected()
+  ],
+  transports : {
+    [mainnet.id] : http("https://eth-mainnet.g.alchemy.com/v2/bSQh--SoswdfzWI_ADH0M")
+  }
+})
