cb5f11ccab490174d8070ee5c1e65d3d43c1e52e
nobitakaif <mk2818356@gmail.com>
2025-09-19 19:56:32 +0000

    added test and stake contracts

--- stake-contract/src/Contract.sol
+++ stake-contract/src/Contract.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract StakeContract {
+    mapping(address => uint) stakes;// storing the data with mapping, this address how much token hold eg kaif -> 1eth
+    uint256 public totalStakes;
+
+    constructor(){
+
+    }
+
+    function stake(uint _amount) public payable{
+        require(_amount>0);
+        require(_amount == msg.value);
+        stakes[msg.sender] += _amount;
+        totalStakes += _amount;
+    }
+
+    function unStake(uint _amount) public{
+        require(stakes[msg.sender] >= _amount);
+        payable(msg.sender).transfer(_amount);
+        totalStakes -= _amount;
+        stakes[msg.sender] -= _amount;
+    }
+}
--- stake-contract/test/Contract.t.sol
+++ stake-contract/test/Contract.t.sol
@@ -3,21 +3,36 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+import "../src/Contract.sol";
 
 contract TestContract is Test {
-    Contract c;
+    StakeContract c;
 
     function setUp() public {
-        c = new Contract();
+        c = new StakeContract();
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testStake() public{
+        uint value = 10 ether;
+        c.stake{value : value }(value);
+        assert(c.totalStakes() == value);
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testUnstake() public{
+        uint value = 10 ether;
+        vm.startPrank(0x171BD3b7239d9dB1DC8Ec6a4440EF17663dc2609); // whenver same person call this who deployed then fails but we try call this with different address
+        vm.deal(0x171BD3b7239d9dB1DC8Ec6a4440EF17663dc2609, value); 
+        c.stake{value : value}(value);
+        c.unStake(value);
+        assert(c.totalStakes() == 0);
     }
+
+    // function testBar() public {
+    //     assertEq(uint256(1), uint256(1), "ok");
+    // }
+
+    // function testFoo(uint256 x) public {
+    //     vm.assume(x < type(uint128).max);
+    //     assertEq(x + x, x * 2);
+    // }
 }
