29415cbdd3dec818a31c09d39123c418a3470822
mohd kaif <mk2818356@gmail.com>
2025-10-02 18:14:03 +0530

    traits

--- rust/four/src/main.rs
+++ rust/four/src/main.rs
@@ -22,13 +22,39 @@ impl Summary for User{
 
 impl Fix for User {}
 
+struct SecondUser{
+    username : String,
+}
+
+trait PrintUser{
+    fn print(&self) -> String{
+        return String::from("something happens wrong maybe, that's why default one is executed");
+    }
+}
+
+impl PrintUser for SecondUser{
+    // fn print(&self)->String{
+    //     return format!("name of the second user is {}",self.username);
+    // }
+}
+
 fn main() {
     let user = User{
         name : String::from("nobita"),
         age : 32
     };
+
+    let second_user = SecondUser{
+        username : String::from("kaif"),
+    };
+
+    print_display(second_user);
     // println!("{}",user.summarize());
     notify(user);
+}
+
+fn print_display<T: PrintUser>(str : T){
+    println!("{}",str.print());
 }
 
 fn notify<T : Summary + Fix>(u : T){ // now we use fix() and summarize() because Summary and Fix implementation abstract User struct  
