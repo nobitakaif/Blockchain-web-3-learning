1b8a9c86085334644717cdca5f231e3f89c22e73
mohd kaif <mk2818356@gmail.com>
2025-10-09 16:46:13 +0530

    learn some diff way to serialization and deserialization

--- rust/borsh/src/main.rs
+++ rust/borsh/src/main.rs
@@ -1,4 +1,4 @@
-use borsh::{BorshSerialize, BorshDeserialize};
+use borsh::{BorshSerialize, BorshDeserialize, to_vec};
 
 #[derive(BorshDeserialize,BorshSerialize, Debug)]
 struct User{
@@ -14,6 +14,23 @@ fn main() {
     
     let mut vec : Vec<u8> = Vec::new(); // defining vec array 
     let ans = user.serialize(&mut vec); // putting the number(bits & bytes) into array user.serialize(args[]) will convert into 0-255 numbers and put inside args[]
+
+    let user2 = User{
+        username : String::from("nobitakaif"),
+        password : String::from("nobitakaif")
+    };
+
+    let mut bytes = to_vec(&user2); // diff way to serialization with pattern matching we can unwrap() it and make thread panic.
+    match bytes{
+        Ok(arr) => {
+            println!("to_vec {:?}",arr);
+            let vec2_back_obj = User::try_from_slice(&arr).unwrap(); // make unwrap() so thread will panic and it'll not print 
+            println!("vec_to_obj{:?}",vec2_back_obj);
+        }
+        Err(_) => println!("error while serializaiting")
+    }
+
+    
 
     match ans{
         Ok(_) => println!("serialization {:?}", vec),
