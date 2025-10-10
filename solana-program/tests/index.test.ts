8c5e2976a93aa5225dc010b39c8aabb3516848aa
mohd kaif <mk2818356@gmail.com>
2025-10-10 20:09:21 +0530

    initialising instruction & transaction for create account

--- solana-program/tests/index.test.ts
+++ solana-program/tests/index.test.ts
@@ -1,9 +1,12 @@
 
 import { expect, test } from "bun:test"
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js"
+import { COUNTER_SIZE } from "./types/types";
 
 let countrAccountKeypair  = Keypair.generate(); // for store the data on the blockchain
 let adminKeypair  = Keypair.generate(); // for admin or user wallet 
+
+const programId= new PublicKey("GdoFqYrwWWuHghoi1VX5JHWiBBheps3mnoSYM5oVNYRS")
 
 test("program initializing",async ()=>{
     const connection = new Connection("http://localhost:8899") // connecting to the custom rpc url i.e running on the our terminal
@@ -14,5 +17,23 @@ test("program initializing",async ()=>{
     await connection.confirmTransaction(txn)
     const balance = await connection.getBalance(adminKeypair.publicKey)
     console.log(balance)
+
+
+    const minimumAmount = await connection.getMinimumBalanceForRentExemption(COUNTER_SIZE) // checking how much solana need to be hold in this account to store the data(bytes) on the blockchain 
+    console.log("minimum ammount", minimumAmount / LAMPORTS_PER_SOL )
     // expect(balance).toBe(5*)
+
+    const instruction = SystemProgram.createAccount({
+        fromPubkey : adminKeypair.publicKey,
+        lamports : minimumAmount,
+        space : COUNTER_SIZE,
+        programId : programId,
+        newAccountPubkey : countrAccountKeypair.publicKey
+    })
+
+    const createAccountTxn = new Transaction()
+    createAccountTxn.add(instruction);
+    const signature = await connection.sendTransaction(createAccountTxn, [adminKeypair, countrAccountKeypair])
+    await connection.confirmTransaction(signature)
+    console.log(countrAccountKeypair.publicKey.toBase58)
 })
