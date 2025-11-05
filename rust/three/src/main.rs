3a8a15225191862b2fbe63f4cbc3cdf3ef1d8526
mohd kaif <mk2818356@gmail.com>
2025-11-05 23:42:56 +0530

    learn about external crates

--- rust/three/src/main.rs
+++ rust/three/src/main.rs
@@ -1,7 +1,7 @@
 use chrono::prelude::*; // importing chrono library form docs.rs similar as npmjs registry, * means import everything 
 // use chorno::Utc; // just importing Utc this library let you do date/time realted things
 use dotenv::dotenv;
-use std::env;
+use std::{env, fs};
 
 fn main(){
     let utc = Utc::now(); // it tells you utc time 12:00
@@ -10,9 +10,15 @@ fn main(){
 
     dotenv().ok(); // reading .env file acceptance
 
-    let http_server = env::var("http"); // it return you Result<String, Error>
+    let http_server = env::var("https"); // it return you Result<String, Error>
     // let http_wraped = http_server.unwrap(); //.upwrap() will throw if https is not present in the .env file and don't exicute code
     // print!("{}",http_wraped);
+    let read_file = fs::read_to_string("Cargo.tomls");
+    match read_file{
+        Ok(val)=> print!("{}", val),
+        Err(_)=> print!("something went wrong while reading the content")
+    }
+
     match http_server{
         Ok(str) => println!("your server is running on {}", str), // if successed 
         Err(e)=> println!("Error while reading .env file") // if not
