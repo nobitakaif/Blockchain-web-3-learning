77437875174ee04d9063fb51c1f71271b7cd0ddf
mohd kaif <mk2818356@gmail.com>
2025-12-11 23:35:10 +0530

    explore tanstack query

--- eth-wagmi-wallet-adapter/src/App.tsx
+++ eth-wagmi-wallet-adapter/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+async function sendRequest(){
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+  return res.data
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Posts/>
+    </QueryClientProvider>
   )
 }
+
+function Posts(){
+  // const client  = useQueryClient()
+  const {data, isLoading, error} = useQuery({queryKey : ["todo"], queryFn:sendRequest})
+  if(error){
+    return <div>
+      <h3>Error while fetching the data</h3>
+    </div>
+  }
+  if(isLoading){
+    return "Loading..."
+  }
+  return <div>
+        {JSON.stringify(data)}
+  </div>
+}
+
 
 export default App
