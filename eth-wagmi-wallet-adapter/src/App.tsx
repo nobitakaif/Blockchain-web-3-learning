9a9804cadb7805541aaa66229ea6fb5cc54d60aa
mohd kaif <mk2818356@gmail.com>
2025-12-14 23:30:24 +0530

    build send eth functionality

--- eth-wagmi-wallet-adapter/src/App.tsx
+++ eth-wagmi-wallet-adapter/src/App.tsx
@@ -1,28 +1,31 @@
 import { Input } from "./components/ui/input";
 import { createConfig, http, injected, WagmiProvider } from "wagmi"
-import { mainnet, base } from "wagmi/chains"
+import { mainnet, base, sepolia } from "wagmi/chains"
 import { Button } from "./components/ui/button";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SendEth from "./components/sendEth";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 const querClient = new QueryClient()
 
 const config = createConfig({
-  chains : [mainnet, base],
+  chains : [sepolia, base],
   connectors : [
     injected()
   ],
   transports:{
-    [mainnet.id] :http(),
+    [sepolia.id] :http(),
     [base.id] : http()
   }
 })
 
 
 export default function App(){
-  return <WagmiProvider config={config}>
-    <QueryClientProvider client={querClient} >
-      <SendEth/>
-    </QueryClientProvider>
-  </WagmiProvider>
+  return <ThemeProvider defaultTheme="dark">
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={querClient} >
+        <SendEth/>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </ThemeProvider> 
 }
--- eth-wagmi-wallet-adapter/src/components/sendEth.tsx
+++ eth-wagmi-wallet-adapter/src/components/sendEth.tsx
@@ -1,11 +1,75 @@
+import { useAccount, useBalance, useConnect, useSendTransaction,  } from "wagmi";
+import {formatUnits, parseEther, type Address} from "viem"
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { Check, Copy } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip"
+import { useRef, useState,  } from "react";
 
 export default function SendEth(){
-    return <div className="h-screen w-full flex items-center justify-center flex-col">
-        <div className="h-72 w-72 flex flex-col gap-3 rounded-lg  border p-2">
-            <Input placeholder="enter "></Input>
-            <Button >send 0.1 eth</Button>
+    const { address } = useAccount();
+    const { connectors, connect } = useConnect(); // this hooks comes from wagmi, which let you do to connect the application with wallet, 
+    const  balance = useBalance({address : address})
+    const [ isCopy, setIsCopy ] = useState(true)
+    const { data : hash, sendTransaction} = useSendTransaction()
+    const amountRef = useRef<null | HTMLInputElement>(null)
+    const toRef = useRef<null | HTMLInputElement>(null)
+    function  sendEth(){
+        sendTransaction({
+            to : toRef.current?.value! as Address,
+            value :parseEther(amountRef.current?.value!)
+        })
+    }
+    const addressRef = useRef<null | HTMLInputElement>(null)
+    // formatUnits(balance.data?.value!, 18)
+    return <div className="h-screen w-full flex items-center justify-center flex-col gap-2">
+        
+        <div className="flex flex-col gap-2  items-center  border-black rounded-xl w-104 h-88 justify-center">
+            <div className="flex items-center gap-1">
+                <Input type="text" value={address} className="w-91 font-bold " disabled ref={addressRef}>
+                    {/* <Copy/> */}
+                </Input>
+                { isCopy ? <div className="h-full flex justify-center items-center cursor-pointer" onClick={()=>{
+                        navigator.clipboard.writeText(addressRef.current!.value)
+                        setIsCopy(false)
+                        setTimeout(()=>{
+                            setIsCopy(true)
+                        },3000)
+                    }}>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Copy className="h-8 w-6 mt-1 text-gray-500"/>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Copy</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        
+                    </div> 
+                : 
+                    <div>
+                        <Check className="text-green-400"/>
+                    </div>
+                }
+            </div>
+            <div className="w-full">
+                <Button variant={"link"} > {Number(balance.data?.value)/1e18} eth </Button> 
+            </div>
+            <div className=" w-100 flex flex-col gap-3 rounded-lg border p-2">
+                <Input placeholder="enter eht..." ref={amountRef}></Input>
+                <Input placeholder="to..." ref={toRef}/>
+                <Button onClick={sendEth} >send 0.1 eth</Button>
+                {connectors.map((connector)=>( // iterating all the wallet which we had imported in App.tsx eg. injected, bckp, phnt
+                    <Button 
+                        key={connector.id}
+                        onClick={()=> connect({connector})} 
+                        className="bg-blue-500"
+                    >
+                        {connector.name}
+                    </Button>
+                ))}
+            </div>
         </div>
     </div>
 }
