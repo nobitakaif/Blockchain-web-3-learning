fb7e89362a611fd83f9e0f114741ac426ff70272
mohd kaif <mk2818356@gmail.com>
2026-01-02 15:36:04 +0530

    trading backend

--- trading_bot/backend/index.ts
+++ trading_bot/backend/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import { Keypair } from "@solana/web3.js"
+import express from "express"
+    import { PrismaAdapter } from "@prisma/adapter-postgres"
+import { PrismaClient } from "./generated/prisma/client"
+
+const app = express()
+
+    const adapter = new PrismaAdapter(process.env.DATABASE_URL as string)
+    const client = new PrismaClient({
+    adapter
+})
+app.post("/signup", async(req,res)=>{
+    const email = req.body.email
+    const password = req.body.password
+    // do zod validation 
+
+    const keypair = new Keypair()
+    const resposne = await client.user.create({
+        data: {
+            email,
+            password,
+            publicKey: keypair.publicKey.toString(),
+            privateKey: keypair.secretKey.toString()
+        }
+    })
+    res.json(resposne)
+})
