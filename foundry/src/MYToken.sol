0206b08c25d1715cd0d1f9d197efe91b5844b210
mohd kaif <mk2818356@gmail.com>
2026-01-02 15:33:50 +0530

    updated foundry

--- foundry/src/MYToken.sol
+++ foundry/src/MYToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.5;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { console } from "forge-std/console.sol";
 
 contract MyToken is ERC20{
